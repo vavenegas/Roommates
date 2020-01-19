2e83f62a94c44ba49fe19cbf41d217508ae8d61f
Laura Camacho <lcamac02@calpoly.com>
2020-01-18 22:49:37 -0800

    added 2 new layouts

---
+++ app/src/main/java/com/example/personalinforoommate/onOffCampus.kt
@@ -0,0 +1,12 @@
+package com.example.personalinforoommate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class onOffCampus : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_off_campus)
+    }
+}
---
+++ app/src/main/java/com/example/personalinforoommate/questionsPage.kt
@@ -0,0 +1,12 @@
+package com.example.personalinforoommate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class questionsPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions_page)
+    }
+}
