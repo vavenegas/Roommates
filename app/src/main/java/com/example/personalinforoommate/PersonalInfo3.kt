309d02bd2ecda0875f8b1fa51ee5d3bdd1414b96
Valentina Venegas <valentinavenegas@169-231-101-78.wireless.ucsb.edu>
2020-01-18 22:25:03 -0800

    UFinder

--- app/src/main/java/com/example/personalinforoommate/PersonalInfo3.kt
+++ app/src/main/java/com/example/personalinforoommate/PersonalInfo3.kt
@@ -5,18 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class PersonalInfo3 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var counter = 0
-        button1.setOnClickListener {
+        FinishButtonL3.setOnClickListener {
             /*counter = counter + 1
             textView1.text = counter.toString()*/
-            val intent = Intent(this, Main2Activity::class.java)
+            val intent = Intent(this, SummaryofPI4::class.java)
             startActivity(intent)
+
+            val input = fullNameEditTextL3.text
         }
     }
 }
--- app/src/main/java/com/example/personalinforoommate/SummaryofPI4.kt
+++ app/src/main/java/com/example/personalinforoommate/SummaryofPI4.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main2.*
 
-class Main2Activity : AppCompatActivity() {
+class SummaryofPI4 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
