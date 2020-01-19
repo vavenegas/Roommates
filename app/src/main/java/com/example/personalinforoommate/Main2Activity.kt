7719af948d7c88fa3f4eb3f7247be2e3e7c25773
Valentina Venegas <valentinavenegas@169-231-101-78.wireless.ucsb.edu>
2020-01-18 16:50:48 -0800

    first commit

---
+++ app/src/main/java/com/example/personalinforoommate/Main2Activity.kt
@@ -0,0 +1,15 @@
+package com.example.personalinforoommate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        editText.text
+    }
+}
