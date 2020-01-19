d8cc192aee87b99f6392c7c3bc1c15a23bf2df12
Lihong Wu <kansiscawu@outlook.com>
2020-01-19 00:39:00 -0700

    Lihong Wu added new layouts

---
+++ app/src/main/java/com/example/personalinforoommate/Register.kt
@@ -0,0 +1,13 @@
+package com.example.personalinforoommate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.jar.Attributes
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
